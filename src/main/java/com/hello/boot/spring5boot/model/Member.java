9511558b6a4a6fd78cc1aa266bf2152c11b9dd8b
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-07 10:38:35 +0900

    회원가입 : Member 클래스 추가

---
+++ src/main/java/com/hello/boot/spring5boot/model/Member.java
@@ -0,0 +1,22 @@
+package com.hello.boot.spring5boot.model;
+
+
+import lombok.*;
+
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Member {
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String phone;
+    private String regdate;
+}
