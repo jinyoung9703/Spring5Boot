0af4fe01b6c8c1a7cb607fb342eb278a973d6220
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-07 12:00:48 +0900

    회원가입 : MemberMapper 단위테스트 추가

---
+++ src/test/java/com/hello/boot/spring5boot/MemberMapperUnitTest.java
@@ -0,0 +1,36 @@
+package com.hello.boot.spring5boot;
+
+
+import com.hello.boot.spring5boot.dao.MemberDAO;
+import com.hello.boot.spring5boot.dao.MemberDAOImpl;
+import com.hello.boot.spring5boot.model.Member;
+import com.hello.boot.spring5boot.mybatis.MemberMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MemberMapperUnitTest {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    @DisplayName("MemberMapper insert Test")
+    void insertMember(){
+        Member m = new Member(null,"","","",
+                "","","","","",null);
+
+        int result = memberMapper.insertMember(m);
+        System.out.println(result);
+        assertEquals(result,1);
+    }
+
+}
