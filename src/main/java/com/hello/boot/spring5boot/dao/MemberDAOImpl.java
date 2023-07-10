bcf3a00fbecce2528c1c50242a9a96ab3dcad6d6
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-10 15:46:28 +0900

    아이디 중복 검사 :  Mapper, MemberDAO 구현

--- src/main/java/com/hello/boot/spring5boot/dao/MemberDAOImpl.java
+++ src/main/java/com/hello/boot/spring5boot/dao/MemberDAOImpl.java
@@ -40,4 +40,9 @@ public class MemberDAOImpl implements MemberDAO{
 
         return memberMapper.findZipcode(dong);
     }
+
+    @Override
+    public int selectOneUserid(String uid) {
+        return memberMapper.selectOneUserid(uid);
+    }
 }
