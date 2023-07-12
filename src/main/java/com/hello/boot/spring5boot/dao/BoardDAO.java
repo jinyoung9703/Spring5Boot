3864923a868f0bcbd7504c0509a00d8ad8e9d94a
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-12 10:14:49 +0900

    게시판 : boardDAO 구현

---
+++ src/main/java/com/hello/boot/spring5boot/dao/BoardDAO.java
@@ -0,0 +1,16 @@
+package com.hello.boot.spring5boot.dao;
+
+import com.hello.boot.spring5boot.model.Board;
+
+import java.util.List;
+
+
+public interface BoardDAO {
+
+    int insertBoard(Board b);
+
+    List<Board> selectBoard();
+
+    Board selectOneBoard(String bno);
+
+}
---
+++ src/main/java/com/hello/boot/spring5boot/dao/BoardDAOImpl.java
@@ -0,0 +1,33 @@
+package com.hello.boot.spring5boot.dao;
+
+import com.hello.boot.spring5boot.model.Board;
+import com.hello.boot.spring5boot.mybatis.BoardMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("bdao")
+@RequiredArgsConstructor
+public class BoardDAOImpl implements BoardDAO {
+
+    //@Autowiried 없이 DI 구현
+    final BoardMapper boardMapper;
+
+    @Override
+    public int insertBoard(Board b) {
+        return 0;
+    }
+
+    @Override
+    public List<Board> selectBoard() {
+
+        return boardMapper.selectBoard();
+    }
+
+    @Override
+    public Board selectOneBoard(String bno) {
+        return null;
+    }
+}
