8e3b76bb3719c03c3d7915b1a3b12d2559826843
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-13 16:12:03 +0900

    오타수정

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -4,106 +4,107 @@ let agree2 = document.querySelector("#agree2");
 let okagree = document.querySelector("#okagree");
 let noagree = document.querySelector("#noagree");
 
-okagree?.addEventListener('click',() => {
-    if(!agree1.checked) alert('이용약관에 동의하세요!');
-    else if(!agree2.checked) alert('개인정보 이용 동의에 체크하세요!');
-    else location.href = "/join/checkme";
+okagree?.addEventListener('click', () => {
+    if (!agree1.checked) alert('이용약관 동의에 체크하세요!');
+    else if (!agree2.checked) alert('개인정보 이용 동의에 체크하세요!');
+    else location.href = '/join/checkme';
 });
-noagree?.addEventListener('click',() => {
+noagree?.addEventListener('click', () => {
     location.href = "/";
 });
 
-//checkme
-let frm = document.forms.checkfrm2;
+// checkme
+let frm = document.forms.agreefrm2;
 let checkbtn2 = document.querySelector("#checkbtn2");
 let cancelbtn2 = document.querySelector("#cancelbtn2");
-checkbtn2?.addEventListener('click',()=>{
-    if (frm.name.value === '') alert('이름를 입력하세요!!');
-    else if(frm.jumin1.value === '') alert('주민번호를 입력하세요!!');
-    else if(frm.jumin2.value === '') alert('나머지 주민번호를 입력하세요!!');
-    else if(!frm.infoagree.checked) alert('주민번호 처리에 동의하세요!!');
+checkbtn2?.addEventListener('click', () => {
+    if (frm.name.value === '') alert('이름을 입력하세요!!');
+    else if (frm.jumin1.value === '') alert('주민번호를 입력하세요!!');
+    else if (frm.jumin2.value === '') alert('나머지 주민번호를 입력하세요!!');
+    else if (!frm.infoagree.checked) alert('주민번호 처리에 동의하세요!!');
     else {
         frm.method = 'post';
         frm.submit();
     }
+});
+cancelbtn2?.addEventListener('click', () => {
+    location.href = '/';
+});
 
-})
-cancelbtn2?.addEventListener('click',()=>{
-    location.href = "/";
-})
 // joinme
-let fzipbtn= document.querySelector("#findzipbtn");
-let zipbtn= document.querySelector("#zipbtn");
+let fzipbtn = document.querySelector("#findzipbtn");
+let zipbtn = document.querySelector("#zipbtn");
 let dong = document.querySelector("#dong");
 let zipmodal = document.querySelector("#zipmodal");
+
 let addrlist = document.querySelector("#addrlist");
 let sendzip = document.querySelector("#sendzip");
-let modal = null;   // 우편번호 모달(검색)
+let modal = null;    // 우편번호 모달
 
 let email3 = document.querySelector("#email3");
 
 // 우편번호 검색 모달창 띄우기
-zipbtn?.addEventListener('click', ()=> {
-    while(addrlist.lastChild){
+zipbtn?.addEventListener('click', () => {
+    while(addrlist.lastChild) {
         addrlist.removeChild(addrlist.lastChild);
-    }// 이전 검색 결과 지움
-    dong.value = '';    // 이전 검색 키워드 지움
+    } // 이전 검색 결과 지움
+    dong.value = '';     // 이전 검색 키워드 지움
 
     try {
         // 새로운 모달 창 생성
         modal = new bootstrap.Modal(zipmodal, {});
-    }catch (e){}
+    } catch (e) { }
 
-    modal.show();     // 모달창 띄우기
+    modal.show();    // 모달창 띄우기
 });
 
 // 검색한 우편번호 결과 출력
 const showzipaddr = (jsons) => {
     jsons = JSON.parse(jsons); // 문자열을 json객체로 변환
     let addr = '';
-    jsons.forEach(function (data,idx){ // josn 반복처리
-        //주소의 번지가 null인 경우 비칸 처림
+
+    jsons.forEach(function (data, idx) { // json 반복처리
+        // 주소의 번지가 null인 경우 처리
         let bunji = (data['bunji'] !== null) ? data['bunji'] : '';
 
         addr += `<option>${data['zipcode']} ${data['sido']} 
-            ${data['gugun']} ${data['dong']} ${bunji} </option>`;
+            ${data['gugun']} ${data['dong']} ${bunji}</option>`;
     });
     addrlist.innerHTML = addr;
 };
 
-//우편번호 검색
-fzipbtn?.addEventListener('click',() =>{
-   if (dong.value === '') {
-       alert('동이름을 입력하세요!!');
-       return; //안해도 나옴?
-   }
-   const url= '/join/zipcode?dong='+dong.value;
-   fetch(url).then(response => response.text())
-       // .then(text => console.log(text)) 콘솔창에 확인하는방법
-       .then(text => showzipaddr(text))
+// 우편번호 검색
+fzipbtn?.addEventListener('click', () => {
+    if (dong.value === '') {
+        alert('동이름을 입력하세요!!');
+        return;
+    }
+    const url = '/join/zipcode?dong=' + dong.value;
+    fetch(url).then(response => response.text())
+        .then(text => showzipaddr(text));
 });
 
 // 주소 선택하고 닫기
-sendzip?.addEventListener('click',()=>{
+sendzip?.addEventListener('click', () => {
     let frm = document.forms.joinfrm;
-    let addr = addrlist.value;  // 선택한 주소 항목
-    if (addr !==''){
-        // 123-456 서웅ㄹ 관악구 신림동
-        let zip = addr.split(' ')[0];   //우편번호 추출
-        let vaddr =
-            `${addr.split(' ')[1]} ${addr.split(' ')[2]} ${addr.split(' ')[3]}`;  //주소 추출
+    let addr = addrlist.value;   // 선택한 주소 항목
+    if (addr !== '') {
+        // 123-456 서울 관악구 신림동
+        let zip = addr.split(' ')[0];  // 우편번호 추출
+        let addrs = addr.split(' ');
+        let vaddr = `${addrs[1]} ${addrs[2]} ${addrs[3]}`; // 주소추출
 
         frm.zip1.value = zip.split('-')[0];
         frm.zip2.value = zip.split('-')[1];
         frm.addr1.value = vaddr;
 
-        modal.hide();   // 모달창 닫음
-    }else{
+        modal.hide();    // 모달창 닫음
+    } else {
         alert('주소를 선택하세요!!');
     }
 });
 
-//전자우편 주소 선택
+// 전자우편 주소 선택
 email3?.addEventListener('click', () => {
     let frm = document.forms.joinfrm;
     if (email3.value === '직접입력하기') {
@@ -116,27 +117,29 @@ email3?.addEventListener('click', () => {
 });
 
 // 우편번호 검색 엔터키 입력 차단
-dong?.addEventListener('keydown',(e) => {
-     if(e.keyCode === 13)        // 엔터키(13)가 입력되면
-        e.preventDefault();     // 이벤트 전파 방지
+dong?.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13)    // 엔터키(13)가 입력되면
+        e.preventDefault();  // 이벤트 전파 방지
 });
 
-//비밀번호 확인
+// 비밀번호 확인
 let pwd = document.querySelector("#pwd");
 let repwd = document.querySelector("#repasswd");
 let pwdmsg = document.querySelector("#pwdmsg");
 
-repwd?.addEventListener('blur',()=>{
+repwd?.addEventListener('blur', () => {
     let pmsg = '비밀번호가 서로 일치하지 않습니다!!';
     pwdmsg.className = 'text-danger';
 
-    if (pwd.value === repwd.value){
+    if (pwd.value === repwd.value) {
         pmsg = '비밀번호가 서로 일치합니다!!';
         pwdmsg.className = 'text-primary';
     }
+
     pwdmsg.innerText = pmsg;
 });
-// 아이디 중복검사
+
+// 아이디 중복 검사
 let userid = document.querySelector("#uid");
 let checkuid = document.querySelector("#checkuid");
 let uidmsg = document.querySelector("#uidmsg");
@@ -146,26 +149,26 @@ const styleCheckuid = (chkuid) => {
     uidmsg.className = 'text-danger';
     checkuid.value = 'no';
 
-    if (chkuid === '0'){
+    if (chkuid === '0') {
         umsg = '사용 가능한 아이디입니다!!';
         uidmsg.className = 'text-primary';
         checkuid.value = 'yes';
     }
+
     uidmsg.innerText = umsg;
 };
 
 // 아이디 중복 검사
-userid?.addEventListener('blur',() =>{
+userid?.addEventListener('blur', () => {
     if (userid.value === '') {
         uidmsg.innerText = '6~16 자의 영문 소문자, 숫자와 특수기호(_)만 사용할 수 있습니다';
         uidmsg.className = 'text-warning';
         checkuid.value = 'no';
-        return; //안해도 나옴?
+        return;
     }
-    const url= '/join/checkuid/'+userid.value;
+    const url = '/join/checkuid/' + userid.value;
     fetch(url).then(response => response.text())
-        // .then(text => console.log(text)) 콘솔창에 확인하는방법
-        .then(text => styleCheckuid(text))
+        .then(text => styleCheckuid(text));
 });
 
 // 회원정보 저장
@@ -193,15 +196,13 @@ joinbtn?.addEventListener('click', () => {
         frm.submit();
     }
 
-
 });
 
 // joinok
 let go2idx = document.querySelector("#go2idx");
-go2idx?.addEventListener('click', () =>{
-    location.href ='/';
+go2idx?.addEventListener('click', () => {
+    location.href = '/';
 });
-
 
 // login
 let loginbtn = document.querySelector("#loginbtn");
@@ -210,66 +211,59 @@ let lgpwd = document.querySelector("#passwd");
 let lgfrm = document.querySelector("#lgnfrm");
 
 loginbtn?.addEventListener('click', () => {
-    if (lguid.value === '') alert('아이디 입력하세요!!');
-    else if (lgpwd.value === '') alert('비밀번호 입력하세요!!');
-    else {
+   if (lguid.value === '') alert('아이디 입력하세요!!');
+   else if (lgpwd.value === '') alert('비밀번호 입력하세요!!');
+   else {
         lgfrm.method = 'post';
         lgfrm.action = '/join/login';
         lgfrm.submit();
-    }
+   }
 });
 
-//logout
+// logout
 let lgoutbtn = document.querySelector("#lgoutbtn");
 
-lgoutbtn?.addEventListener('click',()=>{
-    location.href='/join/logout';
+lgoutbtn?.addEventListener('click', () => {
+    location.href = '/join/logout';
 });
 
 // board view
-// let newbdbtn = document.querySelector("#newbdbtn");
-// let modbdbtn = document.querySelector("#modbdbtn");
-// let rmvbdbtn = document.querySelector("#rmvbdbtn");
-// let lstbdbtn = document.querySelector("#lstbdbtn");
+let newbdbtn = document.querySelector("#newbdbtn");
+let modbdbtn = document.querySelector("#modbdbtn");
+let rmvbdbtn = document.querySelector("#rmvbdbtn");
+let lstbdbtn = document.querySelector("#lstbdbtn");
+
+newbdbtn?.addEventListener('click', ()=>{
+    location.href = '/board/write';
+});
+
+modbdbtn?.addEventListener('click', ()=>{
+    if (confirm("정말 수정하시겠어요?")) {
+        alert('아직 미지원 기능입니다!!');
+    }
+});
+
+rmvbdbtn?.addEventListener('click', ()=>{
+    if (confirm("정말 삭제하시겠어요?")) {
+        alert('아직 미지원 기능입니다!!');
+    }
+});
+
+lstbdbtn?.addEventListener('click', ()=>{
+    location.href = '/board/list/1';
+});
+
+// board write
 let wrtbdbtn = document.querySelector("#wrtbdbtn");
 
-wrtbdbtn?.addEventListener('click',()=>{
+wrtbdbtn?.addEventListener('click', () => {
     let frm = document.forms.bdfrm;
 
-    if (frm.title.value === '') alert("제목을 작성하세요!!");
-    else if (frm.contents.value === '') alert("본문을 작성하세요!!");
-    else if (grecaptcha.getResponse() === '') alert("자동쓰기방지!!");
+    if (frm.title.value === '') alert('제목을 작성하세요!!');
+    else if (frm.contents.value === '') alert('본문을 작성하세요!!');
+    else if (grecaptcha.getResponse() === '') alert('자동쓰기방지를 체크하세요!!');
     else {
         frm.method = 'post';
         frm.submit();
     }
 });
-
-newbdbtn?.addEventListener('click',()=>{
-    location.href='/board/write';
-});
-
-modbdbtn?.addEventListener('click',()=>{
-    if(confirm("정말 수정하시겠어요?")){
-        alert('아직 미지원 기능입니다!!');
-    }
-});
-
-rmvbdbtn?.addEventListener('click',()=>{
-    if(confirm("정말 삭제하시겠어요?")){
-        alert('아직 미지원 기능입니다!!');
-    }
-});
-
-lstbdbtn?.addEventListener('click',()=>{
-    location.href='/board/list/1';
-});
-
-// board write
-let cancelbtn3 = document.querySelector("#cancelbtn3");
-
-cancelbtn3?.addEventListener('click',()=> {
-   history.back();
-});
-
-
