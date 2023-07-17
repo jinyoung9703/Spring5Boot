c233ac69d4fb3c1ca41f8527d3e962c2a571cf51
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-17 13:41:18 +0900

    자료실 - PdsController : list,view 구현 / write 단위테스트

--- src/main/java/com/hello/boot/spring5boot/controller/PdsController.java
+++ src/main/java/com/hello/boot/spring5boot/controller/PdsController.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,12 +25,22 @@ public class PdsController {
 
     final PdsService psrv;
 
-    @GetMapping("/list")
-    public String pds(){
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg){
         logger.info("pds/list 호출!!");
 
-        return "list-";
+        m.addAttribute("pds",psrv.readpds(cpg));
+        m.addAttribute("cpg",cpg);
+        m.addAttribute("cntpg",psrv.countPds());
+        m.addAttribute("stpg", ((cpg -1) /10 ) * 10 +1);
+
+
+        if (cpg  > (int)m.getAttribute("cntpg"))
+            return "redirect:/pds/list/1";
+
+        return "pds/list";
     }
+
     @GetMapping("/write")
     public String write(){
         logger.info("pds/write 호출!!");
@@ -38,7 +50,7 @@ public class PdsController {
     }
 
     @PostMapping("/write")
-    public String writeok(Pds p, MultipartFile attach){
+    public String writeok(Pds p, MultipartFile attach){ /* 이름명칭이 같아야함 */
         logger.info("pds/writeok 호출!!");
 
         String returnPage = "redirect:/pds/fail";
@@ -49,12 +61,19 @@ public class PdsController {
         // 알아낸 글번호를 이용해 첨부파일 처리 (디비저장, 업로드)
         if (!attach.isEmpty()) {  // 첨부파일이 존재한다면
             psrv.newPdsAttach(attach, pno);
-
             returnPage = "redirect:/pds/list/1";
         }
 
         return returnPage;
     }
 
+    @GetMapping("/view/{pno}")
+    public String view(Model m, @PathVariable String pno){
+        logger.info("pds/view 호출!!");
+
+        m.addAttribute("p",psrv.readOnePds(pno));
+
+        return "pds/view";
+    }
 
 }
