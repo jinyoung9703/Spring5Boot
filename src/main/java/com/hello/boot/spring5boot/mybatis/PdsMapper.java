d4068e2e2ccd54d580aa53c0fb9bdf962aa32487
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-19 13:59:09 +0900

    댓글 - insertPdsReply 구현

--- src/main/java/com/hello/boot/spring5boot/mybatis/PdsMapper.java
+++ src/main/java/com/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -29,6 +29,8 @@ public interface PdsMapper {
 
     List<PdsComment> selectPdsComment(String pno);
 
+    int insertPdsReply(PdsComment pc);
+
     /*int updateViewPds(String bno);
 
     List<Pds> selectFindPds(Map<String, Object> params);
