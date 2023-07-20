e1633435b9f2435ef499a88700a67c16fba46407
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-20 11:17:53 +0900

    갤러리 : Gallery / GalAttach 클래스 추가

---
+++ src/main/java/com/hello/boot/spring5boot/model/GalAttach.java
@@ -0,0 +1,16 @@
+package com.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GalAttach {
+
+    private String gano;
+    private String gno;
+    private String fname;
+    private String fsize;
+}
