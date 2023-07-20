a925d5b7cc5ecc2d5774ec838806352f8740b505
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-20 11:18:29 +0900

    갤러리 : Gallery 매퍼 추가

---
+++ src/main/java/com/hello/boot/spring5boot/mybatis/GalleryMapper.java
@@ -0,0 +1,36 @@
+package com.hello.boot.spring5boot.mybatis;
+
+
+import com.hello.boot.spring5boot.model.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Mapper
+public interface GalleryMapper {
+    int insertGallery(Gallery g);
+
+    int insertGalAttach(GalAttach ga);
+
+    List<Gallery> selectGallery(int stnum);
+
+    Gallery selectOneGallery(String gno);
+
+    int selectCountGallery();
+
+    GalAttach selectOneGalleryAttach(String gno);
+
+//    int insertPdsComment(PdsComment pc);
+//
+//    List<PdsComment> selectPdsComment(String pno);
+//
+//    int insertPdsReply(PdsComment pc);
+
+    /*int updateViewPds(String bno);
+
+    List<Pds> selectFindPds(Map<String, Object> params);
+
+    int countFindPds(Map<String, Object> params);*/
+
+}
