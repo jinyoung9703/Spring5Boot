f40c5a9ea04c3ed87b3e40c69272fba49288b0bb
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-21 10:52:46 +0900

    갤러리 : write, writeok 구현

--- src/main/java/com/hello/boot/spring5boot/controller/GalleryController.java
+++ src/main/java/com/hello/boot/spring5boot/controller/GalleryController.java
@@ -1,5 +1,7 @@
 package com.hello.boot.spring5boot.controller;
 
+import com.hello.boot.spring5boot.model.Gallery;
+import com.hello.boot.spring5boot.model.Pds;
 import com.hello.boot.spring5boot.service.GalleryService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -8,7 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 
 @Controller
@@ -20,20 +26,48 @@ public class GalleryController {
 
     Logger logger = LogManager.getLogger(GalleryController.class);
 
-    @GetMapping("/list{cpg}")
-    public String list(Model m, @PathVariable Integer cpg){
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg) {
         logger.info("gallery/list 호출!!");
 
-        m.addAttribute("gal",gsrv.readGallery(cpg));
-        m.addAttribute("cpg",cpg);
-        m.addAttribute("cntpg",gsrv.countGallery());
-        m.addAttribute("stpg", ((cpg -1) /10 ) * 10 +1);
+        m.addAttribute("gal", gsrv.readGallery(cpg));
+        m.addAttribute("cpg", cpg);
+        m.addAttribute("cntpg", gsrv.countGallery());
+        m.addAttribute("stpg", ((cpg - 1) / 10) * 10 + 1);
 
-
-//        if (cpg  > (int)m.getAttribute("cntpg"))
+//        if (cpg > (int)m.getAttribute("cntpg"))
 //            return "redirect:/gallery/list/1";
 
         return "gallery/list";
+    }
+
+
+
+    @GetMapping("/write")
+    public String write(){
+        logger.info("gallery/write 호출!!");
+
+
+        return "gallery/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Gallery g, List<MultipartFile> attachs){ /* 이름명칭이 같아야함 */
+        logger.info("gallery/writeok 호출!!");
+
+        String returnPage = "redirect:/gallery/fail";
+
+        // 작성한 게시글을 먼저 디비에 저장하고 글번호를 알아냄
+        int gno = gsrv.newGallery(g);
+
+        // 알아낸 글번호를 이용해 첨부파일 처리 (디비저장, 업로드)
+        if (!attachs.isEmpty()) {  // 첨부파일이 존재한다면
+            gsrv.newGalAttach(attachs, gno);
+
+            returnPage = "redirect:/gallery/list/1";
+        }
+
+        return returnPage;
     }
 
 
